6fb7aa4b3fa5dd977c07e3f473998e3186d9172b
Anton Kondralev <motovolg-1@yandex.ru>
2020-05-16 15:12:25 +0400

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,16 @@
 package com.company;
 
 import java.util.Scanner;
+public class Main
+{
 
+    public static void main(String[] args)
+    {
+        System.out.println("Ваша задача угадать число.");
+        int range = 10;
+        int namber = (int) (Math.random() * range);
+        while
+    }
 
 
 
