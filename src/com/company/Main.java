e15563cf84208fe43aaf927e657092fa70bbba52
Anton Kondralev <motovolg-1@yandex.ru>
2020-05-16 14:15:59 +0400

    дописываем кулькулятор

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Ведите операцию");
         System.out.println("1 Сложение");
         System.out.println("2 Вычитание");
         System.out.println("3 Умножение");
         System.out.println("4 Деление");
         Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
         System.out.println("Введите перовое число");
         int a = scanner.nextInt();
         System.out.println("Введите второе число");
         int b = scanner.nextInt();
         int result;
-	// write your code here
+        if(operation == 1)
+        {result = a + b;}
+        else if (operation == 2)
+        {result = a - b;}
+        else if (operation == 3)
+        {result = a * b;}
+       else
+            {result = a/b;}
+        System.out.println("Результат = " + result);
     }
 }
