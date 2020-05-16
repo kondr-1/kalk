f4f2505adfad96c04cdec669f46bf09c5a803f33
Anton Kondralev <motovolg-1@yandex.ru>
2020-05-16 13:45:57 +0400

    настройка калькулятора

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Хрень");
-        System.out.println("Хрень два");
+        System.out.println("Ведите операцию");
+        System.out.println("1 Сложение");
+        System.out.println("2 Вычитание");
+        System.out.println("3 Умножение");
+        System.out.println("4 Деление");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите перовое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+        int result;
 	// write your code here
     }
 }
